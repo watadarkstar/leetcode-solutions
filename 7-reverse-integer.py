c5f27076bb117999b453c398605d3617725a3658
Adrian Carolli <adrian.carolli1@gmail.com>
2016-03-31 11:11:46 -0400

    problem 7 solution

---
+++ 7-reverse-integer.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 0:
+            num = -1*int(str(-1*x)[::-1])
+        else:
+            num = int(str(x)[::-1])
+        
+        if num < -2147483647 or num > 2147483647:
+            return 0
+        else:
+            return num
